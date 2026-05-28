deb2e98a38d7e69e6e0e1eaf7d6be87a4aae35cb
Nolhan Pieroni <70480609+Non0reo@users.noreply.github.com>
2026-05-28 16:41:44 +0200

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css';
+import '@videojs/html/video/player';
+import '@videojs/html/video/skin';
+import '@videojs/html/media/hls-video';
+
+// https://iptv-org.github.io/?q=is_closed%3Afalse+streams%3A%3E0+country%3AFR
+// https://github.com/schumijo/iptv/blob/main/fr.m3u8
